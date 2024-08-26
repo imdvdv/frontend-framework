61d55d5d5e8f918858cc4300db30fa56e7812fec
Igor Medvedev <91053682+imdvdv@users.noreply.github.com>
2024-08-27 00:10:02 +0600

    fixed input class name error

--- assets/js/funcs/form/showHidePassword.js
+++ assets/js/funcs/form/showHidePassword.js
@@ -3,7 +3,7 @@ export function showHidePassword (){
         const eyeIcons = document.querySelectorAll(".form__input-icon_eye");
         eyeIcons.forEach((eyeIcon) => {
             eyeIcon.addEventListener("click", () => {
-                const inputPassword = eyeIcon.parentElement.querySelector(".form__input-value"); 
+                const inputPassword = eyeIcon.parentElement.querySelector(".form__input"); 
                 if (inputPassword.type === "password") {
                     eyeIcon.classList.replace("fa-eye-slash", "fa-eye");
                     return (inputPassword.type = "text");
